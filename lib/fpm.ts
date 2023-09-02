b7079c789a2d2d28dd5cecbf6efe3787a82f0ca7
Heejoon Kang <haseo.hackers@gmail.com>
2023-09-02 21:32:39 +0900

    Extract subcommand from cmd args

--- lib/fpm.ts
+++ lib/fpm.ts
@@ -1,5 +1,6 @@
 import { FpmContext } from './context/mod.ts'
 import { std_flags } from './deps.ts'
+import { FpmLogger, Logger } from './logger/mod.ts'
 
 /**
  * The entry point of the `fpm` CLI application.
@@ -24,9 +25,29 @@ export function fpm(args: string[]) {
       verbose: false,
       debug: false,
     },
+    unknown: (arg) => {
+      if (arg.startsWith('-')) {
+        const logger = new FpmLogger(Logger.standard())
+        logger.error(`Unknown option: ${arg}`)
+        Deno.exit(1)
+      }
+    },
   })
 
   const context = FpmContext.fromFlags(flags)
   const { logger } = context
   logger.debug('flags=', flags)
+
+  if (flags._.length === 0) {
+    logger.error('No command specified.')
+    Deno.exit(1)
+  }
+
+  const subcommand = flags._[0]
+  const subcommandArgs = flags._.slice(1)
+  const otherFlags = flags['--']
+
+  logger.debug('subcommand=', subcommand)
+  logger.debug('subcommandArgs=', subcommandArgs)
+  logger.debug('otherFlags=', otherFlags)
 }
