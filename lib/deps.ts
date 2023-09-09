16eaa5eec228d85bd7fc079a0b118a5624f60678
Heejoon Kang <haseo.hackers@gmail.com>
2023-09-09 10:25:48 +0900

    Add test cases

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,3 +1,4 @@
+export * as std_async from 'https://deno.land/std@0.201.0/async/mod.ts'
 export * as std_flags from 'https://deno.land/std@0.201.0/flags/mod.ts'
 export * as std_fs from 'https://deno.land/std@0.201.0/fs/mod.ts'
 export * as std_path from 'https://deno.land/std@0.201.0/path/mod.ts'
