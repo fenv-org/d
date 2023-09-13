f0619507765db3acd8cf5d348a61c7f3bb4d533e
Heejoon Kang <haseo.hackers@gmail.com>
2023-09-13 09:28:49 +0900

    Add test cases

--- lib/util/src/chain.test.ts
+++ lib/util/src/chain.test.ts
@@ -1,6 +1,6 @@
 import { std } from '../../deps.ts'
 import { assertEquals } from '../../test_deps.ts'
-import { Chain } from './chain.ts'
+import { AsyncChain, Chain } from './chain.ts'
 
 Deno.test('Chain.of', () => {
   assertEquals(Chain.of(1).value, 1)
@@ -22,6 +22,10 @@ Deno.test('Chain.with', () => {
 
 Deno.test('Chain.empty', () => {
   assertEquals(Chain.empty<number>().value, undefined)
+})
+
+Deno.test('Chain.flatten', () => {
+  assertEquals(Chain.flatten(Chain.of(Chain.of(1))).value, 1)
 })
 
 Deno.test('Chain.tap', () => {
@@ -69,8 +73,117 @@ Deno.test('Chain.flatMapAsync', async () => {
     await Chain.of(1)
       .flatMapAsync<number>(async (it) => {
         await std.async.delay(0)
-        return Chain.of<number>(it + 1).toAsync()
+        return AsyncChain.of<number>(Promise.resolve(it + 1))
       })
+      .promise,
+    2,
+  )
+})
+
+Deno.test('AsyncChain.of', async () => {
+  assertEquals(await AsyncChain.of(Promise.resolve(1)).promise, 1)
+  assertEquals(await AsyncChain.of(1).promise, 1)
+  assertEquals(await AsyncChain.of(Promise.resolve(null)).promise, null)
+  assertEquals(await AsyncChain.of(undefined).promise, undefined)
+})
+
+Deno.test('AsyncChain.ofNullable', async () => {
+  assertEquals(await AsyncChain.ofNullable(Promise.resolve(1)).promise, 1)
+  assertEquals(await AsyncChain.ofNullable(1).promise, 1)
+  assertEquals(
+    await AsyncChain.ofNullable<number>(Promise.resolve(null)).promise,
+    undefined,
+  )
+  assertEquals(await AsyncChain.ofNullable<number>(null).promise, undefined)
+  assertEquals(
+    await AsyncChain.ofNullable<number>(Promise.resolve(undefined)).promise,
+    undefined,
+  )
+  assertEquals(
+    await AsyncChain.ofNullable<number>(undefined).promise,
+    undefined,
+  )
+})
+
+Deno.test('AsyncChain.with', async () => {
+  assertEquals(await AsyncChain.with(() => Promise.resolve(1)).promise, 1)
+  assertEquals(await AsyncChain.with(() => 1).promise, 1)
+  assertEquals(await AsyncChain.with(() => Promise.resolve(null)).promise, null)
+  assertEquals(await AsyncChain.with(() => undefined).promise, undefined)
+})
+
+Deno.test('AsyncChain.empty', async () => {
+  assertEquals(await AsyncChain.empty<number>().promise, undefined)
+})
+
+Deno.test('AsyncChain.flatten', async () => {
+  assertEquals(
+    await AsyncChain.flatten(AsyncChain.of(AsyncChain.of(1))).promise,
+    1,
+  )
+  assertEquals(
+    await AsyncChain.flatten(AsyncChain.of(Chain.of(1))).promise,
+    1,
+  )
+  assertEquals(
+    await AsyncChain.flatten(Chain.of(AsyncChain.of(1))).promise,
+    1,
+  )
+})
+
+Deno.test('AsyncChain.tapAsync', async () => {
+  let value: number | undefined = undefined
+  await AsyncChain.of(1).tapAsync((it) => (value = it)).promise
+  assertEquals(value, 1)
+  await AsyncChain.of(2)
+    .tapAsync(async (it) => {
+      await std.async.delay(0)
+      value = it
+    })
+    .promise
+  assertEquals(value, 2)
+})
+
+Deno.test('AsyncChain.mapAsync', async () => {
+  assertEquals(
+    await AsyncChain.of(1)
+      .mapAsync(async (it) => {
+        await std.async.delay(0)
+        return it + 1
+      })
+      .promise,
+    2,
+  )
+})
+
+Deno.test('AsyncChain.flatMapAsync', async () => {
+  assertEquals(
+    await AsyncChain.of(1)
+      .flatMapAsync<number>(async (it) => {
+        await std.async.delay(0)
+        return AsyncChain.of<number>(Promise.resolve(it + 1))
+      })
+      .promise,
+    2,
+  )
+  assertEquals(
+    await AsyncChain.of(1)
+      .flatMapAsync<number>(async (it) => {
+        await std.async.delay(0)
+        return Chain.of<number>(it + 1)
+      })
+      .promise,
+    2,
+  )
+  assertEquals(
+    await AsyncChain.of(1)
+      .flatMapAsync<number>((it) => Chain.of<number>(it + 1))
+      .promise,
+    2,
+  )
+  assertEquals(
+    await AsyncChain.of(1)
+      .flatMapAsync<number>((it) => AsyncChain.of<number>(it + 1))
       .promise,
     2,
   )
--- lib/util/src/chain.ts
+++ lib/util/src/chain.ts
@@ -19,13 +19,17 @@ export class Chain<T> {
     return Chain.ofNullable<T>(undefined)
   }
 
+  static flatten<T>(chain: Chain<Chain<T>>): Chain<T> {
+    return chain.flatMap((it) => it)
+  }
+
   readonly #value: T
 
   public get value(): T {
     return this.#value
   }
 
-  public tap(fn: (value: T) => void): Chain<T> {
+  public tap(fn: (value: T) => unknown): Chain<T> {
     fn(this.#value)
     return this
   }
@@ -62,36 +66,51 @@ export class AsyncChain<T> {
 
   readonly #promise: Promise<T>
 
-  public static of<T>(promise: Promise<T>): AsyncChain<T> {
-    return new AsyncChain(promise)
-  }
-
-  public static ofNullable<T>(
-    promise: Promise<T | null | undefined>,
-  ): AsyncChain<T | undefined> {
+  public static of<T>(promise: Promise<T> | T): AsyncChain<T> {
     return new AsyncChain(
-      promise.then((value) =>
-        value === null || value === undefined ? undefined : value
-      ),
+      promise instanceof Promise ? promise : Promise.resolve(promise),
     )
   }
 
-  public static with<T>(fn: () => Promise<T>): AsyncChain<T> {
-    return new AsyncChain(fn())
+  public static ofNullable<T>(
+    promise: Promise<T | null | undefined> | T | null | undefined,
+  ): AsyncChain<T | undefined> {
+    return new AsyncChain(
+      promise instanceof Promise
+        ? promise.then((value) =>
+          value === null || value === undefined ? undefined : value
+        )
+        : promise === null || promise === undefined
+        ? Promise.resolve(undefined)
+        : Promise.resolve(promise),
+    )
+  }
+
+  public static with<T>(fn: () => Promise<T> | T): AsyncChain<T> {
+    return AsyncChain.of(fn())
   }
 
   public static empty<T>(): AsyncChain<T | undefined> {
-    return AsyncChain.ofNullable<T>(Promise.resolve(undefined))
+    return AsyncChain.ofNullable<T>(undefined)
+  }
+
+  public static flatten<T>(
+    chain:
+      | AsyncChain<AsyncChain<T>>
+      | Chain<AsyncChain<T>>
+      | AsyncChain<Chain<T>>,
+  ): AsyncChain<T> {
+    return chain.flatMapAsync((it) => it)
   }
 
   public get promise(): Promise<T> {
     return this.#promise
   }
 
-  public tapAsync(fn: (value: T) => void | Promise<void>): AsyncChain<T> {
+  public tapAsync(fn: (value: T) => unknown | Promise<unknown>): AsyncChain<T> {
     return new AsyncChain(this.#promise.then(async (value) => {
       const voidOrPromise = fn(value)
-      if (voidOrPromise) {
+      if (voidOrPromise instanceof Promise) {
         await voidOrPromise
       }
       return value
