be9bb49ef1f03b69b50db7089d9f4ee95f5d4c41
Heejoon Kang <haseo.hackers@gmail.com>
2023-09-22 12:38:29 +0900

    Logger v2 (#11)

---
+++ lib/logger/src/logger_v2.test.ts
@@ -0,0 +1,222 @@
+import { assertEquals, Buffer } from '../../test_deps.ts'
+import { createStyles } from '../mod.ts'
+import { createLoggerV2 } from './logger_v2.ts'
+
+const LINE_FEED = Deno.build.os === 'windows' ? '\r\n' : '\n'
+
+function bufferToString(buffer: Buffer): string {
+  const decoder = new TextDecoder()
+  return decoder.decode(buffer.bytes())
+}
+
+Deno.test('supportColors & verbose & debug', async (t) => {
+  let now = 0
+  const stdout = new Buffer()
+  const stderr = new Buffer()
+  const logger = createLoggerV2({
+    stdout,
+    stderr,
+    supportColors: true,
+    verboseEnabled: true,
+    debugEnabled: true,
+    now: () => now,
+  })
+  const styles = createStyles({ supportColors: true })
+
+  await t.step('verbose', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout({ verbose: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      styles.gray.bold('[10m 30.000s]') + ' ' +
+        '[' + styles.brightYellow.bold('test') + ']   ' +
+        styles.brightGreen.bold('└>') + ' ' +
+        '$ Hello ' + styles.bgBlue.italic.bold('world') + LINE_FEED,
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('debug', () => {
+    // advance 123 milliseconds
+    now += 123
+    logger.stderr({ debug: true, timestamp: true })
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stderr))
+    assertEquals(
+      bufferToString(stderr),
+      styles.gray.bold('[10m 30.123s]') + ' ' +
+        '[' + styles.brightYellow.bold('test') + ']   ' +
+        styles.brightGreen.bold('└>') + ' ' +
+        styles.magenta('$') + styles.magenta(' ') +
+        styles.magenta('Hello') + styles.magenta(' ') +
+        styles.bgBlue.italic.bold('world') + LINE_FEED,
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('simple message', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout().push('Hello world').lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      'Hello world' + LINE_FEED,
+    )
+  })
+})
+
+Deno.test('!supportColors & verbose & debug', async (t) => {
+  let now = 0
+  const stdout = new Buffer()
+  const stderr = new Buffer()
+  const logger = createLoggerV2({
+    stdout,
+    stderr,
+    supportColors: false,
+    verboseEnabled: true,
+    debugEnabled: true,
+    now: () => now,
+  })
+
+  await t.step('verbose', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout({ verbose: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      '[10m 30.000s] [test]   └> $ Hello world' + LINE_FEED,
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('debug', () => {
+    // advance 123 milliseconds
+    now += 123
+    logger.stderr({ debug: true, timestamp: true })
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stderr))
+    assertEquals(
+      bufferToString(stderr),
+      '[10m 30.123s] [test]   └> $ Hello world' + LINE_FEED,
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('simple message', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout().push('Hello world').lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      'Hello world' + LINE_FEED,
+    )
+  })
+})
+
+Deno.test('supportColors & !verbose & !debug', async (t) => {
+  let now = 0
+  const stdout = new Buffer()
+  const stderr = new Buffer()
+  const logger = createLoggerV2({
+    stdout,
+    stderr,
+    supportColors: true,
+    now: () => now,
+  })
+
+  await t.step('verbose', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout({ verbose: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    assertEquals(bufferToString(stdout), '')
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('debug', () => {
+    // advance 123 milliseconds
+    now += 123
+    logger.stderr({ debug: true, timestamp: true })
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    assertEquals(bufferToString(stderr), '')
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('simple message', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout().push('Hello world').lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(bufferToString(stdout), 'Hello world' + LINE_FEED)
+  })
+})
