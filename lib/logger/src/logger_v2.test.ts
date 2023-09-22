963fe65475b26e03ce9f1ace258558e0bd5d904c
Heejoon Kang <haseo.hackers@gmail.com>
2023-09-22 12:16:26 +0900

    Add `logger_v2`

---
+++ lib/logger/src/logger_v2.test.ts
@@ -0,0 +1,164 @@
+import { assertEquals, Buffer } from '../../test_deps.ts'
+import { createStyles } from '../mod.ts'
+import { createLoggerV2 } from './logger_v2.ts'
+
+function bufferToString(buffer: Buffer): string {
+  const decoder = new TextDecoder()
+  return decoder.decode(buffer.bytes())
+}
+
+Deno.test('supportColors & verbose & debug', async (t) => {
+  let now = 0
+  const stdout = new Buffer()
+  const stderr = new Buffer()
+  const logger = createLoggerV2({
+    stdout,
+    stderr,
+    supportColors: true,
+    verboseEnabled: true,
+    debugEnabled: true,
+    now: () => now,
+  })
+  const styles = createStyles({ supportColors: true })
+
+  await t.step('verbose', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout({ verbose: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      styles.gray.bold('[10m 30.000s]') + ' ' +
+        '[' + styles.brightYellow.bold('test') + ']   ' +
+        styles.brightGreen.bold('└>') + ' ' +
+        '$ Hello ' + styles.bgBlue.italic.bold('world') + '\n',
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('debug', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stderr({ debug: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stderr))
+    assertEquals(
+      bufferToString(stderr),
+      styles.gray.bold('[21m 00.000s]') + ' ' +
+        '[' + styles.brightYellow.bold('test') + ']   ' +
+        styles.brightGreen.bold('└>') + ' ' +
+        styles.magenta('$') + styles.magenta(' ') +
+        styles.magenta('Hello') + styles.magenta(' ') +
+        styles.bgBlue.italic.bold('world') +
+        '\n',
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('simple message', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout().push('Hello world').lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      'Hello world\n',
+    )
+  })
+})
+
+Deno.test('!supportColors & verbose & debug', async (t) => {
+  let now = 0
+  const stdout = new Buffer()
+  const stderr = new Buffer()
+  const logger = createLoggerV2({
+    stdout,
+    stderr,
+    supportColors: false,
+    verboseEnabled: true,
+    debugEnabled: true,
+    now: () => now,
+  })
+
+  await t.step('verbose', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout({ verbose: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      '[10m 30.000s] [test]   └> $ Hello world\n',
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('debug', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stderr({ debug: true })
+      .timestamp()
+      .package('test')
+      .indent()
+      .childArrow()
+      .pushCharCode(36)
+      .pushCharCode(32)
+      .push('Hello')
+      .push(' ')
+      .push((s) => s.bgBlue.italic.bold('world'))
+      .lineFeed()
+    console.log(bufferToString(stderr))
+    assertEquals(
+      bufferToString(stderr),
+      '[21m 00.000s] [test]   └> $ Hello world\n',
+    )
+  })
+
+  stdout.reset()
+  stderr.reset()
+
+  await t.step('simple message', () => {
+    // advance 10 minutes 30 seconds
+    now += 1000 * 60 * 10 + 1000 * 30
+    logger.stdout().push('Hello world').lineFeed()
+    console.log(bufferToString(stdout))
+    assertEquals(
+      bufferToString(stdout),
+      'Hello world\n',
+    )
+  })
+})
---
+++ lib/logger/src/logger_v2.ts
@@ -0,0 +1,298 @@
+import { cliffy } from '../../deps.ts'
+import { LINE_FEED, Stderr, Stdout } from '../../util/mod.ts'
+import { Styling } from '../mod.ts'
+import { createStyles, Styles } from './styling_v2.ts'
+
+export function createLoggerV2(options: {
+  stdout: Stdout
+  stderr: Stderr
+  supportColors: boolean
+  verboseEnabled?: boolean
+  debugEnabled?: boolean
+  now?: () => number
+}): LoggerV2 {
+  return new LoggerV2Impl({
+    ...options,
+    now: options.now ?? Date.now,
+  })
+}
+
+export interface LoggerV2 {
+  readonly colors: Styles
+
+  stdout(options?: {
+    verbose?: boolean
+    debug?: boolean
+    timestamp?: boolean
+  }): LogBuilder
+
+  stderr(options?: {
+    verbose?: boolean
+    debug?: boolean
+    timestamp?: boolean
+  }): LogBuilder
+}
+
+export interface LogBuilder {
+  timestamp(): LogBuilder
+  package(name: string): LogBuilder
+  indent(): LogBuilder
+  childArrow(): LogBuilder
+  push(text: string | ((styles: Styles) => string)): LogBuilder
+  pushCharCode(...args: number[]): LogBuilder
+  lineFeed(): void
+}
+
+class LoggerV2Impl implements LoggerV2 {
+  constructor(options: {
+    stdout: Stdout
+    stderr: Stderr
+    supportColors: boolean
+    verboseEnabled?: boolean
+    debugEnabled?: boolean
+    now: () => number
+  }) {
+    this.#stdout = options.stdout
+    this.#stderr = options.stderr
+    this.#colors = createStyles(options)
+    this.#verboseEnabled = options.verboseEnabled ?? false
+    this.#debugEnabled = options.debugEnabled ?? false
+    this.#now = options.now
+    this.#startAt = options.now()
+  }
+
+  #stdout: Stdout
+  #stderr: Stderr
+  #colors: Styles
+  #verboseEnabled: boolean
+  #debugEnabled: boolean
+  #now: () => number
+  #startAt: number
+
+  get colors(): Styles {
+    return this.#colors
+  }
+
+  stdout(
+    options?: {
+      verbose?: boolean
+      debug?: boolean
+      timestamp?: boolean
+    },
+  ): LogBuilder {
+    return this.#builder(this.#stdout, options)
+  }
+
+  stderr(
+    options?: {
+      verbose?: boolean
+      debug?: boolean
+      timestamp?: boolean
+    },
+  ): LogBuilder {
+    return this.#builder(this.#stderr, {
+      ...options,
+      defaultStyling: this.#colors.magenta,
+    })
+  }
+
+  #builder(
+    stream: Deno.WriterSync,
+    options: {
+      verbose?: boolean
+      debug?: boolean
+      timestamp?: boolean
+      defaultStyling?: Styling
+    } | undefined,
+  ): LogBuilder {
+    const builder = this.#enabled(options)
+      ? new LogBuilderImpl({
+        stream: stream,
+        now: this.#now,
+        startAt: this.#startAt,
+        styles: this.#colors,
+        defaultStyling: options?.defaultStyling,
+        timestamp: false,
+        package: undefined,
+        indent: 0,
+        childArrow: false,
+        buffer: '',
+      })
+      : VoidLogBuilder.instance
+    if (options?.timestamp) {
+      builder.timestamp()
+    }
+    return builder
+  }
+
+  #enabled(
+    options: { verbose?: boolean; debug?: boolean } | undefined,
+  ): boolean {
+    if (
+      !options ||
+      (!options.verbose && !options.debug) ||
+      (options.verbose && this.#verboseEnabled) ||
+      (options.debug && this.#debugEnabled)
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+class VoidLogBuilder implements LogBuilder {
+  static readonly instance = new VoidLogBuilder()
+
+  timestamp(): LogBuilder {
+    return this
+  }
+
+  package(_: string): LogBuilder {
+    return this
+  }
+
+  indent(): LogBuilder {
+    return this
+  }
+
+  childArrow(): LogBuilder {
+    return this
+  }
+
+  push(_: string | ((styles: Styles) => string)): LogBuilder {
+    return this
+  }
+
+  pushCharCode(..._: number[]): LogBuilder {
+    return this
+  }
+
+  lineFeed(): void {}
+}
+
+class LogBuilderImpl implements LogBuilder {
+  constructor(
+    private readonly options: {
+      readonly stream: Deno.WriterSync
+      readonly now: () => number
+      readonly startAt: number
+      readonly styles: Styles
+      readonly defaultStyling: Styling | undefined
+      readonly timestamp: boolean
+      readonly package: string | undefined
+      readonly indent: number
+      readonly childArrow: boolean
+      readonly buffer: string
+    },
+  ) {}
+
+  timestamp(): LogBuilder {
+    return this.options.timestamp
+      ? this
+      : new LogBuilderImpl({ ...this.options, timestamp: true })
+  }
+
+  package(name: string): LogBuilder {
+    return this.options.package === name
+      ? this
+      : new LogBuilderImpl({ ...this.options, package: name })
+  }
+
+  indent(): LogBuilder {
+    return new LogBuilderImpl({
+      ...this.options,
+      indent: this.options.indent + 1,
+    })
+  }
+
+  childArrow(): LogBuilder {
+    return this.options.childArrow
+      ? this
+      : new LogBuilderImpl({ ...this.options, childArrow: true })
+  }
+
+  push(text: string | ((styles: Styles) => string)): LogBuilder {
+    if (typeof text === 'string') {
+      return new LogBuilderImpl({
+        ...this.options,
+        buffer: this.options.buffer + this.#defaultStyling(text),
+      })
+    } else {
+      return new LogBuilderImpl({
+        ...this.options,
+        buffer: this.options.buffer + text(this.#styles),
+      })
+    }
+  }
+
+  pushCharCode(...args: number[]): LogBuilder {
+    const chars = args.map((c) => String.fromCharCode(c)).join('')
+    return this.push(chars)
+  }
+
+  lineFeed() {
+    const segments = []
+
+    if (this.options.timestamp) {
+      segments.push(
+        this.#styles.gray.bold(
+          `[${timestamp(this.options.now() - this.options.startAt)}]`,
+        ),
+      )
+    }
+
+    if (this.options.package) {
+      const packageName = this.#styles.brightYellow.bold(this.options.package)
+      segments.push(`[${packageName}]`)
+    }
+
+    if (this.options.indent > 0) {
+      if (this.options.indent > 1) {
+        segments.push('  '.repeat(this.options.indent - 1))
+      }
+      segments.push(' ')
+    }
+
+    if (this.options.childArrow) {
+      segments.push(this.#styles.brightGreen.bold(
+        `${cliffy.table.border.bottomLeft}>`,
+      ))
+    }
+
+    segments.push(this.options.buffer)
+
+    this.#printLine(segments)
+  }
+
+  get #styles(): Styles {
+    return this.options.styles
+  }
+
+  #printLine(text: string[]): void {
+    this.options.stream.writeSync(
+      textEncoder.encode(text.join(' ') + LINE_FEED),
+    )
+  }
+
+  #defaultStyling(text: string): string {
+    return this.options.defaultStyling?.(text) ?? text
+  }
+}
+
+const textEncoder = new TextEncoder()
+
+function timestamp(elapsedMillis: number): string {
+  const buffer = []
+  let elapsedSeconds = elapsedMillis / 1000.0
+  const elapsedMinutes = Math.floor(elapsedSeconds / 60.0)
+  elapsedSeconds -= elapsedMinutes * 60.0
+  if (elapsedMinutes > 0) {
+    buffer.push(String(elapsedMinutes % 60), 'm ')
+  }
+  buffer.push(
+    elapsedSeconds.toFixed(3).padStart(elapsedMinutes > 0 ? 6 : 1, '0'),
+    's',
+  )
+  return buffer.join('').padStart(11)
+}
