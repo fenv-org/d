09d02a17db839de4787bdf43ab74a283aa324d04
Heejoon Kang <haseo.hackers@gmail.com>
2023-10-21 15:09:53 +0900

    Add `removeEarlyExitOptions()` function

---
+++ lib/command/src/common/early_exit_options.test.ts
@@ -0,0 +1,40 @@
+import { assertEquals } from 'test/deps.ts'
+import { removeEarlyExitOptions } from './early_exit_options.ts'
+
+Deno.test('removeEarlyExitOptions', async (t) => {
+  await t.step('test case1', () => {
+    assertEquals(
+      removeEarlyExitOptions(
+        '--config test-sample/d.yaml -v test --early-exit -r expanded abc --debug'
+          .split(/\s+/),
+      ),
+      [
+        '--config',
+        'test-sample/d.yaml',
+        '-v',
+        'test',
+        '-r',
+        'expanded',
+        'abc',
+        '--debug',
+      ],
+    )
+  })
+
+  await t.step('test case2', () => {
+    assertEquals(
+      removeEarlyExitOptions(
+        '--config=test-sample/d.yaml test --verbose --early-exit -r expanded abc'
+          .split(/\s+/),
+      ),
+      [
+        '--config=test-sample/d.yaml',
+        'test',
+        '--verbose',
+        '-r',
+        'expanded',
+        'abc',
+      ],
+    )
+  })
+})
