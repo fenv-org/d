2681ad4c1dff2064ef1cea7bf1566afe9787bdfc
Heejoon Kang <haseo.hackers@gmail.com>
2023-10-28 19:07:25 +0900

    Remove unnecessary test

--- lib/d.test.ts
+++
@@ -1,9 +0,0 @@
-import { std } from 'deps.ts'
-import { assertEquals } from 'test/deps.ts'
-
-Deno.test('Test the behavior of `path.dirname()`', () => {
-  assertEquals(std.path.dirname('/foo/bar/baz'), '/foo/bar')
-  assertEquals(std.path.dirname('/foo/bar'), '/foo')
-  assertEquals(std.path.dirname('/foo'), '/')
-  assertEquals(std.path.dirname('/'), '/')
-})
